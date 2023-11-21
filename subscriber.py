4530b3f5ec78a35f4864be25f26a034768fdd7c9
Wilker_Tyller <89866520+Wilkerty@users.noreply.github.com>
2023-11-21 20:41:56 -0300

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -3,7 +3,7 @@ from constPS import * #-
 
 context = zmq.Context()
 s = context.socket(zmq.SUB)          # create a subscriber socket
-p = "tcp://"+ HOST +":"+ PORT        # how and where to communicate
+p = "tcp://"+ HOST1 +":"+ PORT        # how and where to communicate
 s.connect(p)                         # connect to the server
 s.setsockopt_string(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
 
