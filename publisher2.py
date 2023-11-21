55d724af87a76f00d3611a0701ce5780bbe05f3c
Wilker_Tyller <89866520+Wilkerty@users.noreply.github.com>
2023-11-21 20:47:02 -0300

    Create publisher2.py

---
+++ publisher2.py
@@ -0,0 +1,21 @@
+import zmq, time
+import random
+from constPS import * #-
+
+def roleta():
+	if (random.randint(1,6)=3)
+		msg = "bang! morestes"
+	else 
+		msg = "esta salvo!"
+	return msg
+
+context = zmq.Context()
+s = context.socket(zmq.PUB)        # create a publisher socket
+p = "tcp:// 0.0.0.0:" + PORT      # how and where to communicate
+s.bind(p)                          # bind socket to the address
+while True:
+	time.sleep(5)                    # wait every 5 seconds
+	msg = str.encode("ROLETA " + roleta())
+		
+	s.send(msg) # publish the current time
+	
