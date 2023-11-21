01394d5ab8a0231bc00b9ab2ba3e33b5427ef952
Wilker_Tyller <89866520+Wilkerty@users.noreply.github.com>
2023-11-21 20:29:40 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,11 +1,17 @@
 import zmq, time
+import random
 from constPS import * #-
 
 context = zmq.Context()
 s = context.socket(zmq.PUB)        # create a publisher socket
-p = "tcp://"+ HOST +":"+ PORT      # how and where to communicate
+p = "tcp:// 0.0.0.0:" + PORT      # how and where to communicate
 s.bind(p)                          # bind socket to the address
 while True:
 	time.sleep(5)                    # wait every 5 seconds
 	msg = str.encode("TIME " + time.asctime())
+	if (random.randint(1,6)=3)
+		msg = str.encode("ROLETA bang! morestes")
+	else 
+		msg = str.encode("ROLETA esta salvo!")
+		
 	s.send(msg) # publish the current time
