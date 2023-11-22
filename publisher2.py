14f8d384ecd8be17ec9c6db0c1306ef55ae019a0
Wilker_Tyller <89866520+Wilkerty@users.noreply.github.com>
2023-11-21 21:02:23 -0300

    Update publisher2.py

--- publisher2.py
+++ publisher2.py
@@ -11,7 +11,7 @@ def roleta():
 
 context = zmq.Context()
 s = context.socket(zmq.PUB)        # create a publisher socket
-p = "tcp:// 0.0.0.0:" + PORT      # how and where to communicate
+p = "tcp://0.0.0.0:" + PORT      # how and where to communicate
 s.bind(p)                          # bind socket to the address
 while True:
 	time.sleep(5)                    # wait every 5 seconds
