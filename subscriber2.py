925d75c6b7ff1d4fcc2c5a3f86d873e540dc2985
Wilker_Tyller <89866520+Wilkerty@users.noreply.github.com>
2023-11-21 21:34:26 -0300

    Update subscriber2.py

--- subscriber2.py
+++ subscriber2.py
@@ -6,6 +6,7 @@ s = context.socket(zmq.SUB)          # create a subscriber socket
 p = "tcp://"+ HOST2 +":"+ PORT        # how and where to communicate
 s.connect(p)                         # connect to the server
 s.setsockopt_string(zmq.SUBSCRIBE, "ROLETA")  # subscribe to ROLETA messages
+s.setsockopt_string(zmq.SUBSCRIBE, "NAIPE")  # subscribe to ROLETA messages
 
 for i in range(5):  # Five iterations
 	time = s.recv()   # receive a message
